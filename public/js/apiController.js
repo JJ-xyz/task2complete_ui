3293cdfd8fd0307b1e8784c3752926af975f2c3d
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-17 21:03:13 -0400

    Small fixes pre-upload to heroku
    Small fixes pre-upload to heroku.

--- public/js/apiController.js
+++ public/js/apiController.js
@@ -6,8 +6,8 @@
 	function ApiController($http, $state){
 
 		var self = this;
-    //var rootUrl = "https://task2complete-api.herokuapp.com";  // API heroku url
-    var rootUrl = "http://localhost:3000";  // API local url
+    var rootUrl = "https://task2complete-api.herokuapp.com";  // API heroku url
+    //var rootUrl = "http://localhost:3000";  // API local url
 
     self.signup = signup;
     self.login = login;
